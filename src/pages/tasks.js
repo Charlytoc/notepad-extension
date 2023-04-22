e3be5708660a49ee96db520a17fcd5b3ca398807
Charlytoc <charlyjchaconc@gmail.com>
2023-04-22 13:53:00 -0500

    add a toggle to done, not-done tasks

--- src/pages/tasks.js
+++ src/pages/tasks.js
@@ -14,23 +14,14 @@ let html = () => {
             location.reload()
         }
     }
-    // actions.handleContent = (e) => {
-    //     task.content = e.target.value;
-    //     if (e.keyCode === 13 && task.title) {
-    //         let data = JSON.parse(localStorage.getItem('tasks')) || []; 
-    //         data.push(task);
-    //         localStorage.setItem('tasks', JSON.stringify(data));
-    //         location.reload()
-    //     }}
-
 
     actions.addNote = (e) => {
-        if (e.keyCode === 13) { // check if Enter key was pressed
-            const inputValue = e.target.value // get the value of the input field and remove any leading/trailing spaces
-            if (inputValue) { // check that the input value is not empty
-                let data = JSON.parse(localStorage.getItem('tasks')) || []; // get the existing data array from localStorage, or initialize an empty array if it doesn't exist
-                data.push(inputValue); // add the input value to the data array
-                localStorage.setItem('tasks', JSON.stringify(data)); // save the updated data array back to localStorage
+        if (e.keyCode === 13) { 
+            const inputValue = e.target.value 
+            if (inputValue) { 
+                let data = JSON.parse(localStorage.getItem('tasks')) || []; 
+                data.push(inputValue); 
+                localStorage.setItem('tasks', JSON.stringify(data)); 
                 location.reload()
                 
             }
@@ -43,12 +34,22 @@ let html = () => {
         localStorage.setItem('tasks', JSON.stringify(tasks))
         location.reload()
     }
+    actions.markAsDone = (e) => {
+        const index = parseInt(e.target.dataset.noteId);
+        tasks[index].class === "done" ? tasks[index].class = "" : tasks[index].class = "done"
+        
+        console.log(tasks[index], index, e.target.dataset)
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+        location.reload()
+    }
 
     const returnSticker = (item, index) => {
-       
-        return `<div >
+        return `<div class="${item.class}">
                     <h3>${item.title}</h3>
+                    <div>
                     <i  data-note-id=${index} class="fa-solid fa-trash erase-note"></i>
+                    <i data-note-id=${index} class="fa-solid fa-square-check mark-as-done"></i>
+                    </div>
                 </div>`
     }
 
@@ -61,7 +62,6 @@ let html = () => {
     <a href="calendar.html" class="link">month goals</a>
     </div>
     <input  id="title-input" placeholder="it's easy to do simple tasks than harder ones!" type="text" />
-
     <section class="sticker-board">
     ${tasks.map((item, index)=> returnSticker(item, index)).join(' ')}
     </section>
@@ -73,6 +73,9 @@ document.addEventListener("render", ()=>{
 
     document.querySelectorAll(".erase-note").forEach((button) => {
         button.addEventListener("click", actions.deleteNote);
+      });
+    document.querySelectorAll(".mark-as-done").forEach((button) => {
+        button.addEventListener("click", actions.markAsDone);
       });
 
 })
